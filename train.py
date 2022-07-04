809a7fb78a37c01b68ef062500cd96cf6b59adbf
Yolanda <qinzhili@outlook.com>
2022-07-04 15:39:51 +0800

    copy from server already down finetune

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
 import argparse
 import importlib
 from utils import *
-from models.trainer import FSCILTrainer
+# from models.trainer import FSCILTrainer
 # from models.base.fscil_trainer import FSCILTrainer
 
 MODEL_DIR=None
 DATA_DIR = 'data/'
-PROJECT='cec' # cec base
+PROJECT='finetune' # cec base
 
 def get_command_line_parser():
     parser = argparse.ArgumentParser()
@@ -18,8 +18,8 @@ def get_command_line_parser():
     parser.add_argument('-dataroot', type=str, default=DATA_DIR)
 
     # about pre-training
-    parser.add_argument('-epochs_base', type=int, default=1)
-    parser.add_argument('-epochs_new', type=int, default=1)
+    parser.add_argument('-epochs_base', type=int, default=100)
+    parser.add_argument('-epochs_new', type=int, default=100)
     parser.add_argument('-lr_base', type=float, default=0.1)
     parser.add_argument('-lr_new', type=float, default=0.1)
     parser.add_argument('-schedule', type=str, default='Step',
@@ -32,19 +32,19 @@ def get_command_line_parser():
     parser.add_argument('-temperature', type=int, default=16)
     parser.add_argument('-not_data_init', action='store_true', help='using average data embedding to init or not')
 
-    parser.add_argument('-batch_size_base', type=int, default=64)
+    parser.add_argument('-batch_size_base', type=int, default=128)
     parser.add_argument('-batch_size_new', type=int, default=0, help='set 0 will use all the availiable training image for new')
     parser.add_argument('-test_batch_size', type=int, default=100)
     parser.add_argument('-base_mode', type=str, default='ft_cos',
                         choices=['ft_dot', 'ft_cos']) # ft_dot means using linear classifier, ft_cos means using cosine classifier
-    parser.add_argument('-new_mode', type=str, default='avg_cos',
+    parser.add_argument('-new_mode', type=str, default='ft_cos',
                         choices=['ft_dot', 'ft_cos', 'avg_cos']) # ft_dot means using linear classifier, ft_cos means using cosine classifier, avg_cos means using average data embedding and cosine classifier
 
     # for episode learning
     parser.add_argument('-train_episode', type=int, default=50)
     parser.add_argument('-episode_shot', type=int, default=5)
     parser.add_argument('-episode_way', type=int, default=5)
-    parser.add_argument('-episode_query', type=int, default=10)
+    parser.add_argument('-episode_query', type=int, default=5)
 
     # for cec
     parser.add_argument('-lrg', type=float, default=0.1) #lr for graph attention network
@@ -71,6 +71,6 @@ if __name__ == '__main__':
     pprint(vars(args))
     args.num_gpu = set_gpu(args)
 
-    # trainer = importlib.import_module('models.%s.fscil_trainer' % (args.project)).FSCILTrainer(args)
-    trainer=FSCILTrainer(args)
+    trainer = importlib.import_module('models.%s.fscil_trainer' % (args.project)).FSCILTrainer(args)
+    # trainer=FSCILTrainer(args)
     trainer.train()
